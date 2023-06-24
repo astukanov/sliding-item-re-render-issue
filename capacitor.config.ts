c67e61bf6c939b2cb3100a0462bc0ad816e5d6b7
alex <alexander.stukanov@gmx.com>
2023-06-24 12:03:40 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sliding-item-re-render-issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
